16f6ad005f09c0feac8dca33bdf16a23ec6fe065
victor-mac <victor.amaro.c@gmail.com>
2018-02-15 19:32:52 -0200

    chore(Project): Project initial structure configuration

---
+++ virtualwallet/virtualwallet/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  virtualwallet
+//
+//  Created by victor-mac on 15/02/18.
+//  Copyright © 2018 victor. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
