40c179d8c43da695202a4a2eb329e0e2276753de
victor-mac <victor.amaro.c@gmail.com>
2018-02-19 21:07:07 -0300

    test(UpdateCurrencyOperation): Add test covering the case of invalid data sent by the server

--- virtualwallet/virtualwalletTests/Network/UpdateCurrencyTest.swift
+++ virtualwallet/virtualwalletTests/Network/UpdateCurrencyTest.swift
@@ -18,6 +18,7 @@ class UpdateCurrencyTest: XCTestCase {
 
     override func tearDown() {
         super.tearDown()
+        OHHTTPStubs.removeAllStubs()
     }
 
     func test_ShouldReturnJSON() throws {
@@ -53,7 +54,7 @@ class UpdateCurrencyTest: XCTestCase {
         wait(for: [updateExpectation], timeout: 5.0)
     }
 
-    func test_ShouldReturnError_HTTP500() {
+    func test_ShouldReturnError_ServerError() {
         let updateExpectation = expectation(description: "Update Currency")
         let expectedError = NetworkError.serverError(statusCode: "500",
                                                      message: "Response status code was unacceptable: 500.")
@@ -64,6 +65,36 @@ class UpdateCurrencyTest: XCTestCase {
 
         stub(condition: isHost(BaseRouter.baseBCDHost)) { _ in
             return OHHTTPStubsResponse(jsonObject: [:], statusCode: 500, headers: ["content-type": "application/json"])
+        }
+
+        let service: RESTService = RESTService(request: request, queue: DispatchQueue.global())
+        let operation: UpdateCurrencyPropertiesOperation = UpdateCurrencyPropertiesOperation(service: service)
+
+        operation.operationDidFinish = { error, info in
+            XCTAssertEqual(error as? NetworkError, expectedError)
+            updateExpectation.fulfill()
+        }
+        let queue: OperationQueue = OperationQueue()
+        queue.addOperation(operation)
+
+        wait(for: [updateExpectation], timeout: 5.0)
+    }
+
+    func test_ShouldReturnError_invalidDataReceived() {
+        let updateExpectation = expectation(description: "Update Currency")
+
+        let urlCurrencyParam: String = "USD"
+        let urlDateParam: Date = Date()
+        let request: CurrencyRouter =
+            CurrencyRouter.retrieveQuotationFor(currencyAcronym: urlCurrencyParam, date: urlDateParam)
+
+        let expectedError = NetworkError.invalidDataReceived(requestDescription: request.requestDescription)
+
+        stub(condition: isHost(BaseRouter.baseBCDHost)) { _ in
+            if let data = "[null]".data(using: String.Encoding.utf8) {
+                return OHHTTPStubsResponse(data: data, statusCode: 200, headers: ["content-type": "application/json"])
+            }
+            return OHHTTPStubsResponse()
         }
 
         let service: RESTService = RESTService(request: request, queue: DispatchQueue.global())
