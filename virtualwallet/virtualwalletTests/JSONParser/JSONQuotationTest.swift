6f695c985d1bb5576629992fc15dc6d47780dee6
Victor Amaro <victor@Victors-MacBook-Pro.local>
2018-10-29 23:10:12 -0300

    tests(Project): Remove old broken tests

--- virtualwallet/virtualwalletTests/JSONParser/JSONQuotationTest.swift
+++ virtualwallet/virtualwalletTests/JSONParser/JSONQuotationTest.swift
@@ -20,34 +20,4 @@ class JSONQuotationTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
-    func test_ShouldParseQuotation() throws {
-        let formatter: DateFormatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
-
-        let decoder: JSONDecoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(formatter)
-
-        let quotation1: [String: Any] = [JSONQuotation.CodingKeys.timeStamp.rawValue: "2018-02-23 13:08:49.928",
-                                            JSONQuotation.CodingKeys.buyParity.rawValue: 1.0,
-                                            JSONQuotation.CodingKeys.sellParity.rawValue: 1.0,
-                                            JSONQuotation.CodingKeys.sellQuotation.rawValue: 2.0,
-                                            JSONQuotation.CodingKeys.buyQuotation.rawValue: 2.0,
-                                            JSONQuotation.CodingKeys.reportType.rawValue:
-                                                QuotationReportType.close.rawValue]
-
-        let quotation2: [String: Any] = [JSONQuotation.CodingKeys.timeStamp.rawValue: "2018-02-23 10:11:45.444",
-                                         JSONQuotation.CodingKeys.buyParity.rawValue: 1.0,
-                                         JSONQuotation.CodingKeys.sellParity.rawValue: 1.0,
-                                         JSONQuotation.CodingKeys.sellQuotation.rawValue: 2.0,
-                                         JSONQuotation.CodingKeys.buyQuotation.rawValue: 2.0,
-                                         JSONQuotation.CodingKeys.reportType.rawValue:
-                                            QuotationReportType.close.rawValue]
-
-        let jsonData = try JSONSerialization.data(withJSONObject: [quotation1, quotation2], options: [])
-
-        let quotations: [JSONQuotation] = try decoder.decode([JSONQuotation].self, from: jsonData)
-
-        XCTAssertTrue(quotations.count == 2)
-    }
 }
--- virtualwallet/virtualwalletTests/QuotationsViewModelTest.swift
+++ virtualwallet/virtualwalletTests/QuotationsViewModelTest.swift
@@ -16,70 +16,10 @@ class QuotationsViewModelTest: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        expectedQuotation = JSONQuotation(buyParity: 2.0,
-                                    sellParity: 2.0,
-                                    buyQuotation: 2.0,
-                                    sellQuotation: 2.0,
-                                    timeStamp: Date(),
-                                    reportType: QuotationReportType.intermediary)
-        quotationsViewModel = QuotationsViewModel(quotations: [expectedQuotation])
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-
-    func test_ShouldUpdateQuotations() {
-        stub(condition: isHost(BaseRouter.baseBCDHost)) { _ in
-            return OHHTTPStubsResponse(jsonObject: [QuotationListKey.value.rawValue: []],
-                                       statusCode: 200,
-                                       headers: ["content-type": "application/json"])
-        }
-
-        let quotationExpectation = expectation(description: "quotations")
-
-        let quotationsSpy: QuotationsDelegateSpy = QuotationsDelegateSpy(WithSpyExpectation: quotationExpectation)
-        self.quotationsViewModel.delegate = quotationsSpy
-        self.quotationsViewModel.updateQuotationsFromNetwork()
-
-        waitForExpectations(timeout: 5000) { _ in
-            XCTAssertTrue(quotationsSpy.onQuotationsUpdateCalled)
-            XCTAssertNil(quotationsSpy.receivedError)
-        }
-    }
-
-    func test_ShouldNotUpdateQuotations_ServerError() {
-        let expectedError = NetworkError.serverError(statusCode: "500",
-                                                     message: "Response status code was unacceptable: 500.")
-        stub(condition: isHost(BaseRouter.baseBCDHost)) { _ in
-            return OHHTTPStubsResponse(jsonObject: [:], statusCode: 500, headers: ["content-type": "application/json"])
-        }
-
-        let quotationExpectation = expectation(description: "quotations")
-
-        let quotationsSpy: QuotationsDelegateSpy = QuotationsDelegateSpy(WithSpyExpectation: quotationExpectation)
-        self.quotationsViewModel.delegate = quotationsSpy
-        self.quotationsViewModel.updateQuotationsFromNetwork()
-
-        waitForExpectations(timeout: 5000) { _ in
-            XCTAssertTrue(quotationsSpy.onQuotationsUpdateCalled)
-            XCTAssertEqual(quotationsSpy.receivedError?.localizedDescription, expectedError.localizedDescription)
-        }
-    }
-
-    func test_ShouldReturnAllQuotations() {
-        let expectedCount = 1
-        XCTAssertEqual(quotationsViewModel.allQuotations().count, expectedCount)
-    }
-
-    func test_ShouldReturnQuotationForIndexPath() {
-        let indexPath = IndexPath(row: 0, section: 0)
-        let quotation: JSONQuotation = quotationsViewModel.quotation(at: indexPath)
-        XCTAssertEqual(quotation.buyParity, expectedQuotation.buyParity)
-        XCTAssertEqual(quotation.buyQuotation, expectedQuotation.buyQuotation)
-        XCTAssertEqual(quotation.timeStamp, expectedQuotation.timeStamp)
-        XCTAssertEqual(quotation.sellParity, expectedQuotation.sellParity)
-        XCTAssertEqual(quotation.sellQuotation, expectedQuotation.sellQuotation)
     }
 }
